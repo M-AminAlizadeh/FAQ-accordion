3b0eb084ed01be83c06d6326cac31e384011fc20
m-aminalizadeh <aminalizadeh@riseup.net>
2024-07-23 12:28:35 +0330

    Delete toggle and try to use open/close class

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const questionContainers = document.querySelectorAll(".question-container");
+const answerContainers = document.querySelectorAll(".answer-container");
 
 questionContainers.forEach((questionContainer) => {
   questionContainer.addEventListener("click", (e) => {
+    // Reset all of them to close
+    answerContainers.forEach((answerContainer) => {
+      answerContainer.classList.add("close");
+      answerContainer.classList.remove("open");
+    })
     const answerContainer = e.target.nextElementSibling;
-    answerContainer.classList.toggle("toggle");
+    answerContainer.classList.add("open");
+    answerContainer.classList.remove("close");
   }); 
 })
