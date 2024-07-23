e35c37c4bef391d2a71c4fbc5ecdff9dfc391b98
m-aminalizadeh <aminalizadeh@riseup.net>
2024-07-23 14:19:50 +0330

    Change the icon while click event each question

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const questionContainers = document.querySelectorAll(".question-container");
 const answerContainers = document.querySelectorAll(".answer-container");
+const questionsIcon = document.querySelectorAll(".question-container img");
+const plusIconPath = "./assets/images/icon-plus.svg";
+const minusIconPath = "./assets/images/icon-minus.svg";
 
 questionContainers.forEach((questionContainer) => {
   questionContainer.addEventListener("click", (e) => {
+    questionsIcon.forEach((icon) => {
+      icon.src = plusIconPath;
+    })
+    e.target.childNodes[1].src = minusIconPath;
     // Reset all of them to close
     answerContainers.forEach((answerContainer) => {
       answerContainer.classList.add("close");
